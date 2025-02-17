5d2ba11b89106983470931666daca613c0aaed9c
Steve Artavia <stvartavia@gmail.com>
2025-02-17 12:09:00 -0600

    actualizacion en funciones dinamicas de contacto

--- assets/js/contenido-dinamico.js
+++ assets/js/contenido-dinamico.js
@@ -2,14 +2,57 @@
 
 let nombreClienteHTML = document.querySelector(".nombre-cliente");
 
-const nombre = `nombre del cliente`;
+const nombre = `nombre del cliente`; // Agregar
 
 nombreClienteHTML.innerHTML = nombre;
 
-// Contacto Whatsapp
-const telefono = ``;
+// Telefono
+const telefono = `89545745`; // Agregar
+
+const numeroDeTelefono = document.querySelector(".fa-phone");
+numeroDeTelefono.href = `tel:${telefono}`;
+
+// Email
+const email = `cliente@gmail.com`;
+
+const urlEmail = `mailto:${email}`
+const enlaceEmail = document.querySelector(".fa-envelope");
+enlaceEmail.href = urlEmail;
+
+// Whatsapp
 const mensaje = `¡Hola ${nombre}! Quiero información sobre tus servicios`;
-const url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+
+const urlWhatsapp = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
 
 const enlaceWhatsapp = document.querySelector('.fa-whatsapp');
-enlaceWhatsapp.href = url;
+enlaceWhatsapp.href = urlWhatsapp;
+
+// Facebook 
+const urlFacebook = ``; // Agregar
+
+const enlaceFacebook = document.querySelector(".fa-facebook-f");
+enlaceFacebook.href = urlFacebook;
+
+// Instagram
+const urlInstagram = ``; // Agregar
+
+const enlaceInstagram = document.querySelector(".fa-instagram");
+enlaceInstagram.href = urlInstagram;
+
+// LinkedIn
+const urlLinkedin = ``; // Agregar
+
+const enlaceLinkedin = document.querySelector(".fa-linkedin");
+enlaceLinkedin.href = urlLinkedin;
+
+// Behance
+const urlBehance = ``; // Agregar
+
+const enlaceBehance = document.querySelector(".fa-behance");
+enlaceBehance.href = urlBehance;
+
+// Twitter
+const urlTwitter = ``; // Agregar
+
+const enlaceTwitter = document.querySelector(".fa-twitter");
+enlaceTwitter.href = urlTwitter;
